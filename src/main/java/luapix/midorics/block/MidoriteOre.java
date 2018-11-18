08c7e90b7de411104c17325283d897b4f05daad7
Luapix <luapixel@gmail.com>
2018-11-18 14:59:18 +0100

    Added localization to Midorite Ore. Made it breakable by iron tools and above, dropping 1 Midorite gem (or more with Fortune).

--- src/main/java/luapix/midorics/block/MidoriteOre.java
+++ src/main/java/luapix/midorics/block/MidoriteOre.java
@@ -1,14 +1,31 @@
 package luapix.midorics.block;
 
+import luapix.midorics.item.ModItems;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+
+import javax.annotation.Nonnull;
+import java.util.Random;
 
 public class MidoriteOre extends BlockBase {
 	public MidoriteOre() {
 		super("midorite_ore", Material.ROCK);
 		setHardness(3f);
-		setResistance(5f);
 		setCreativeTab(CreativeTabs.MATERIALS);
-		
+		setHarvestLevel("pickaxe", 2);
+	}
+	
+	@Override
+	@Nonnull
+	public Item getItemDropped(IBlockState state, Random rand, int fortune) {
+		return ModItems.midoriteGem;
+	}
+	
+	@Override
+	public int quantityDroppedWithBonus(int fortune, @Nonnull Random random) {
+		int i = random.nextInt(fortune + 2);
+		return (i == 0) ? 1 : i;
 	}
 }
