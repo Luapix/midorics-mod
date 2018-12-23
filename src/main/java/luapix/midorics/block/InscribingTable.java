e2eaa551032cf849ed61810633dfd92f133c6d00
Luapix <luapixel@gmail.com>
2018-12-23 16:42:40 +0100

    Added Inscribing Table block (+ model, texture, name), simplified Midorite Ore blockstate JSON.

---
+++ src/main/java/luapix/midorics/block/InscribingTable.java
@@ -0,0 +1,15 @@
+package luapix.midorics.block;
+
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+import net.minecraft.creativetab.CreativeTabs;
+
+public class InscribingTable extends BlockBase {
+	public InscribingTable() {
+		super("inscribing_table", Material.WOOD);
+		setHardness(3F);
+		setSoundType(SoundType.WOOD);
+		setCreativeTab(CreativeTabs.MISC);
+		setHarvestLevel("axe", 0);
+	}
+}
--- src/main/java/luapix/midorics/block/ModBlocks.java
+++ src/main/java/luapix/midorics/block/ModBlocks.java
@@ -9,16 +9,21 @@ import net.minecraftforge.registries.IForgeRegistry;
 public class ModBlocks {
 	@GameRegistry.ObjectHolder("midorics:midorite_ore")
 	public static MidoriteOre midoriteOre;
+	@GameRegistry.ObjectHolder("midorics:inscribing_table")
+	public static InscribingTable inscribingTable;
 	
 	public static void registerBlocks(IForgeRegistry<Block> registry) {
 		registry.register(new MidoriteOre());
+		registry.register(new InscribingTable());
 	}
 	
 	public static void registerItemBlocks(IForgeRegistry<Item> registry) {
 		registry.register(midoriteOre.createItemBlock());
+		registry.register(inscribingTable.createItemBlock());
 	}
 	
 	public static void registerModels() {
 		ModItems.registerItemRenderer(Item.getItemFromBlock(midoriteOre), 0);
+		ModItems.registerItemRenderer(Item.getItemFromBlock(inscribingTable), 0);
 	}
 }
